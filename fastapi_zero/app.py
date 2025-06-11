b616187016e32d9d6d2da1dc944dddaae0bc2ff8
Ricardo Pereira <patresio@gmail.com>
2025-06-11 08:03:05 -0300

    ✨ feat(api): cria endpoint html ola mundo e seu teste

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
 
 from fastapi_zero.schemas import (
     Message,
@@ -13,6 +14,19 @@ from fastapi_zero.schemas import (
 app = FastAPI(title='FastAPI Zero - Curso FastAPI', version='0.1.0')
 
 database = []
+
+
+@app.get('/html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
